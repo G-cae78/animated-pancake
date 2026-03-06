832c3cdbfe952e9df8c47486f82875ae57974d49
G-cae78 <123216765+G-cae78@users.noreply.github.com>
2026-03-06 15:48:03 +0000

    commit

---
+++ HeaderTemplate.h
@@ -0,0 +1,9 @@
+/* Create a header file for each source code file using the same name, but with a "*.h" extension. */
+/* Keep it in the same folder as the source code file, and include it via #include "file.h" */
+
+#ifndef FILENAME /* This symbolic name is unique and should match the file name. */
+#define FILENAME /* This expression makes sure that this header file is only included once. */
+
+/* Add all your function prototypes, macros, #defines, etc. below. */
+
+#endif
