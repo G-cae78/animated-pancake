cd897ba9dc39499637ba59b14600a1aadd13d9d1
G-cae78 <123216765+G-cae78@users.noreply.github.com>
2026-03-09 06:32:42 +0000

    Use struct to hold encryption methods

--- OpenSSL.c
+++ OpenSSL.c
@@ -10,9 +10,20 @@
 struct timespec start, end;
 
 /*  Run with  cd "/Users/"Username"/Documents/Fourth Year/Computer and Network Security/Assignment 2" && \
-    gcc -o OpenSSL OpenSSL.c Functions.c -lcrypto 2>&1*/ 
+    gcc -o OpenSSL OpenSSL.c Functions.c -lcrypto 2>&1 && ./OpenSSL*/ 
+
+    struct OpenSSLSettings
+    {
+        const EVP_CIPHER *cipher;
+    };
+    
 int main (void)
 {
+    /* Initialise the OpenSSL library AES, ARIA and CAMELLIA */
+    struct OpenSSLSettings settings[] = {{EVP_aes_128_ofb()}, 
+                                        {EVP_aria_128_ofb()}, 
+                                        {EVP_camellia_128_ofb()}};
+
     /* Load the human readable error strings for libcrypto */
     ERR_load_crypto_strings();
 
@@ -31,31 +42,35 @@ int main (void)
     int decryptedtext_len, ciphertext_len;
     double elapsed;
 
-    /* Encrypt the plaintext */
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
-    ciphertext_len = encrypt(plaintext, key, ciphertext, EVP_aes_256_cbc());
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
-    elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
-    printf("Encrypt CPU time: %f s\n", elapsed);
-    printf("Ciphertext length is %d:\n", ciphertext_len);
-    BIO_dump_fp(stdout, (const unsigned char *)ciphertext, ciphertext_len);
+    for (int i = 0; i < sizeof(settings) / sizeof(settings[0]); i++) {
+        printf("--------------------------------------------------\n");
+        printf("Using cipher: %s\n", EVP_CIPHER_name(settings[i].cipher));
+        /* Encrypt the plaintext */
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+        ciphertext_len = encrypt(plaintext, key, ciphertext, settings[i].cipher);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+        elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+        printf("Encrypt CPU time: %f s\n", elapsed);
+        printf("Ciphertext length is %d:\n", ciphertext_len);
+        BIO_dump_fp(stdout, (const unsigned char *)ciphertext, ciphertext_len);
 
-    printf("\n");
+        printf("\n");
 
-    /* Decrypt the ciphertext */
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
-    decryptedtext_len = decrypt(ciphertext, key, ciphertext_len, decryptedtext, EVP_aes_256_cbc());
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
-    elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
-    printf("Decrypt CPU time: %f s\n", elapsed);
-    printf("Decrypted text length is %d:\n", decryptedtext_len);
-   
-    decryptedtext[decryptedtext_len] = '\0'; // Null-terminate the decrypted text
-    printf("Decrypted text is:\n%s\n", decryptedtext);
+        /* Decrypt the ciphertext */
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+        decryptedtext_len = decrypt(ciphertext, key, ciphertext_len, decryptedtext, settings[i].cipher);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+        elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+        printf("Decrypt CPU time: %f s\n", elapsed);
+        printf("Decrypted text length is %d:\n", decryptedtext_len);
+    
+        decryptedtext[decryptedtext_len] = '\0'; // Null-terminate the decrypted text
+        printf("Decrypted text is:\n%s\n", decryptedtext);
 
-    /* Clean up */
-    EVP_cleanup();
-    ERR_free_strings();
+        /* Clean up */
+        EVP_cleanup();
+        ERR_free_strings();
+    }
     
     /* 
       Console output should be:
